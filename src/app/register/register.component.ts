3a3cae83170c5b98d832a2c3d80dcdbb30f75542
sandeerth <sandeerth777@gmail.com>
2020-09-07 15:26:28 +0530

    reactive forms and validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,21 +14,44 @@ export class RegisterComponent implements OnInit {
   pwd="";
   pin="";
 
+  registerForm = this.fb.group({
+    name:['',[ Validators.required]],
+    acno:['',[ Validators.required]],
+    pwd:['',[ Validators.required]],
+    pin:['',[ Validators.required]],
+  
+
+  })
+
   constructor(private dataService:DataService,
-    private router:Router) { }
+    private router:Router,
+    private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
+  getError(name){
+    return this.registerForm.get(name).errors
+     
+    }
+  
 register(){
+
+  if (this.registerForm.get('name').errors){
+    alert("name is invalid");
+  }
+  if(this.registerForm.valid){
   const result=this.dataService.register(
-    this.name,
-    this.acno,
-    this.pin,
-    this.pwd);
+    this.registerForm.value.name,
+    this.registerForm.value.acno,
+    this.registerForm.value.pin,
+    this.registerForm.value.pwd);
     if (result){
       alert("successfully created account,please login");
       this.router.navigateByUrl("");
     }
   
+}else{
+  alert("form is invalid");
+}
 }
 }
